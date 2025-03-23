f5367da00bd403b8b532bd22cd5b460338777314
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2025-03-24 02:50:52 +0900

    feat: integration of environmental variables configuration

--- apps/nestwood-batch/src/nestwood-batch.module.ts
+++ apps/nestwood-batch/src/nestwood-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NestwoodBatchController } from './nestwood-batch.controller';
 import { NestwoodBatchService } from './nestwood-batch.service';
-
+import {ConfigModule} from "@nestjs/config"
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot()],
   controllers: [NestwoodBatchController],
   providers: [NestwoodBatchService],
 })
