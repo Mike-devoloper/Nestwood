fc787b4280f7430d6ea0751b972809042a3a7814
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2025-03-25 00:40:08 +0900

    feat: build project standarts

--- apps/nestwood-backend/src/app.module.ts
+++ apps/nestwood-backend/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config"
+import { DatabaseModule } from './database/database.module';
+import { ComponentsModule } from './components/components.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver} from '@nestjs/apollo';
+
 @Module({
-  imports: [
-    ConfigModule.forRoot()],
+  imports: [GraphQLModule.forRoot({
+    driver: ApolloDriver,
+    playground: true,
+    uploads: false,
+    autoSchemaFile: true,
+  }),
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    ComponentsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
