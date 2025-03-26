959a28a3799f8e6adad2b6f0c9fc51c2749e18d7
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2025-03-26 18:28:25 +0900

    feat: create member schema model

--- apps/nestwood-backend/src/components/member/member.service.ts
+++ apps/nestwood-backend/src/components/member/member.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel} from '@nestjs/mongoose';
+import {Model} from "mongoose"
+import { Member } from '../../libs/dto/Member';
 
 @Injectable()
 export class MemberService {
+    constructor(@InjectModel("Member")
+    private readonly memberModel: Model<Member>) {}
+
 
     public async signup():Promise<string> {
         return "signup is execured"
---
+++ apps/nestwood-backend/src/libs/dto/member.ts
@@ -0,0 +1,89 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql"
+import { ObjectId } from "mongoose"
+import { MemberAuth, MemberStatus, MemberType } from "../enums/member.enum"
+
+@ObjectType()
+export class Member {
+    @Field(() => String)
+    _id: ObjectId
+
+    @Field(() => MemberType)
+    memberType: MemberType
+
+    @Field(() => MemberAuth)
+    memberAuthType: MemberAuth
+
+    @Field(() => MemberStatus)
+    memberStatus: MemberStatus
+
+    @Field(() => String)
+    memberNick: string
+
+    @Field(() => String)
+    memberPhone: string
+
+    memberPassword?: string;
+
+    @Field(() => String, {nullable: true})
+    memberFullName?: string
+
+    @Field(() => String)
+    memberImage: string
+
+    @Field(() => String, {nullable: true})
+    memberAddress?: string
+
+    @Field(() => String, {nullable: true})
+    memberDesc?: string
+
+    @Field(() => Int)
+    memberProperties: number
+
+    @Field(() => Int)
+    memberFollowers: number
+
+    @Field(() => Int)
+    memberFollowings: number
+
+    @Field(() => Int)
+    memberArticle: number
+
+    @Field(() => Int)
+    memberViews: number
+
+    @Field(() => Int)
+    memberPoints: number
+
+    @Field(() => Int)
+    memberLikes: number
+
+    @Field(() => Int)
+    memberRank: number
+
+    @Field(() => Int)
+    memberWarnings: number
+
+    @Field(() => Int)
+    memberComments: number
+
+    @Field(() => Int)
+    memberBlocks: number
+
+    @Field(() => Date, {nullable: true})
+    deletedAt?: Date
+
+    @Field(() => Date)
+    updatedAt: Date
+
+    @Field(() => Date)
+    createdAt: Date
+
+    @Field(() => String, {nullable: true})
+    accessToken?: string;
+
+    // @Field(() => [MeLiked], {nullable: true})
+    // meLiked?: MeLiked[]
+
+    // @Field(() => [MeFollowed], {nullable: true})
+    // meFollowed?: MeFollowed[]
+}
